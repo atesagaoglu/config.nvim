7ecb52c28e6c07318b3134eb07288dc6bf9974f7
Mert Emre Ateşağaoğlu <atesagaoglu.mertemre@gmail.com>
2024-01-10 20:35:27 +0300

    git integration

---
+++ lua/atesagaoglu/plugins/neogit.lua
@@ -0,0 +1,15 @@
+return{
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+        "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = function ()
+        local neogit = require('neogit')
+        neogit.setup()
+
+        vim.keymap.set("n", "<leader>vv", function() neogit.open() end)
+        vim.keymap.set("n", "<leader>vc", function() neogit.open({"commit"}) end)
+    end
+}
