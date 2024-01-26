70694a3933d650ce1c6fa0672f71bb85e5d3e263
Mert Emre Ateşağaoğlu <atesagaoglu.mertemre@gmail.com>
2024-01-26 21:32:40 +0300

    remaps from theprimeagen

--- lua/atesagaoglu/core/maps.lua
+++ lua/atesagaoglu/core/maps.lua
@@ -4,3 +4,12 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- move selected line up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
