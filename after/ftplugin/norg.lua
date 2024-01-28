8f06ed511212c0586dfde50c6c3aa9f532d91071
Mert Emre Ateşağaoğlu <atesagaoglu.mertemre@gmail.com>
2024-01-28 22:09:02 +0300

    Norg specific keybinds and options

---
+++ after/ftplugin/norg.lua
@@ -0,0 +1,14 @@
+vim.opt_local.conceallevel = 2
+-- vim.opt_local.concealcursor = "n"
+
+function Concursor()
+   local val = vim.fn.execute('echo &concealcursor')
+   if val == '\n' then
+      vim.opt.concealcursor = "n"
+   else
+      vim.opt.concealcursor = ""
+   end
+end
+
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>ct", [[:Neorg toggle-concealer<CR>]], {noremap = true, silent=true})
+vim.api.nvim_buf_set_keymap(0, "n", "<localleader>cc", [[:lua Concursor()<CR>]], {noremap = true, silent=true})
